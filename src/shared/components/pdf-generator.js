60771252703f2e9ee72a5d11993118b9a7bf3191
sebastian noreña meglan <sebastiannorena@reactiva.com.co>
2021-04-16 17:38:41 -0500

    pdf 1.5/2

--- src/shared/components/pdf-generator.js
+++ src/shared/components/pdf-generator.js
@@ -1,5 +1,6 @@
 import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
+
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 const valuesToJson = (values, type = "list") => {
@@ -15,7 +16,79 @@ const valuesToJson = (values, type = "list") => {
                 stack.push(...[{ text: element.title, style: 'subtitle' }, { text: element.value, style: 'text' }]);
             });
             break;
+        case "education":
+            values.forEach(element => {
+                stack.push(...[
+                    {
+                        columns: [
+                            {
+                                width: '60%',
+                                stack: [
+                                    { text: element.title, style: 'subtitle' },
+                                    { text: element.date, style: 'text' }
 
+                                ]
+                            },
+                            {
+                                width: '40%',
+                                stack: [
+                                    { text: element.institution, style: 'subtitle' },
+                                    { text: element.location, style: 'text' }
+                                ]
+                            }
+                        ]
+                    }
+                ]);
+            });
+            break;
+        case "experience":
+            values.forEach(element => {
+                stack.push(...[
+                    {
+                        columns: [
+                            {
+                                width: '60%',
+                                stack: [
+                                    { text: element.title, style: 'subtitle' },
+                                    { text: element.date, style: 'text' }
+                                ]
+                            },
+                            {
+                                width: '40%',
+                                stack: [
+                                    { text: element.institution, style: 'subtitle' },
+                                    { text: element.location, style: 'text' }
+                                ]
+                            }
+                        ]
+                    }
+                ]);
+            });
+            break;
+        case "competence":
+            let col = [];
+            let count = 1;
+            values.forEach(element => {
+                const item =
+                {
+                    width: '33%',
+                    stack: [
+                        { text: element.title, style: 'subtitle' },
+                        { text: element.value, style: 'text' }
+                    ]
+                }
+                col.push(item);
+                if (count === 3) {
+                    stack.push({ columns: col })
+                    col = [];
+                    count = 0;
+                }
+                count++;
+            });
+            if (count > 1) {
+                stack.push({ columns: col })
+            }
+            break;
         default:
             break;
     }
@@ -24,10 +97,10 @@ const valuesToJson = (values, type = "list") => {
 }
 
 export const GeneratePDF = (data) => {
-    
-      
+
+
     const document = {
-        
+
         content: [
             { text: data.name, style: 'header' },
             { text: data.position, style: 'role' },
@@ -71,9 +144,29 @@ export const GeneratePDF = (data) => {
                             { text: data.profile.title, style: 'title' },
                             " ",
                             { text: data.profile.value, style: 'text' },
+                            " ",
+                            { text: data.education.title, style: 'title' },
+                            " ",
+                            [...valuesToJson(data.education.values, "education")],
+                            " ",
+                            " ",
+                            [...valuesToJson(data.education.values, "education")],
+                            " ",
+                            { text: data.experience.title, style: 'title' },
+                            " ",
+                            [...valuesToJson(data.experience.values, "experience")],
+                            " ",
+                            { text: data.certifications.title, style: 'title' },
+                            " ",
+                            [...valuesToJson(data.certifications.values, "education")],
+                            " ",
+                            { text: data.competences.title, style: 'title' },
+                            " ",
+                            [...valuesToJson(data.competences.values, "competence")],
+                            " ",
                         ],
 
-                    },
+                    }
                 ]
             }
         ],
